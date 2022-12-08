871f2172e8cb50396c5317b0dd55b63004bba453
toffer305 <toffer305@gmail.com>
2022-12-08 11:05:52 -0500

    Added Update Account functionality

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -141,3 +141,17 @@ class TestAccountService(TestCase):
         """
         response = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_account(self):
+        # create an account to update!
+        test_account = AccountFactory()
+        response = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Update the created account
+        new_account = response.get_json()
+        new_account["name"] = "Something Known"
+        response = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_account = response.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
